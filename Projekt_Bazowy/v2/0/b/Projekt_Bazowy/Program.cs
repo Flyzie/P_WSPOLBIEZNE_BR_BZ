69aaa2ee5fa5e498cd199eddeaf3576e8d8fcc7b
BartoszJakis <93491389+BartoszJakis@users.noreply.github.com>
2023-03-03 16:28:02 +0100

    base C# testing

---
+++ Projekt_Bazowy/v2/0/b/Projekt_Bazowy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program0
+{
+    class Program
+    {
+        public long addition(int x, int y)
+        {
+            int BASEsum = x + y;
+            return BASEsum;
+        }
+    }
+}
